729f244038abe1807354ae9610d4253a47e1db64
marijndegen <marijndegen96@hotmail.com>
2019-02-06 14:18:30 +0100

    Update LanguageController.php

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -3,32 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App;
 
 //TODO controleren of de juiste dingen worden weergegeven.
 
 class LanguageController extends Controller
 {
     function select(){
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $dataArray = json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+        $array = json_decode(json_encode($dataArray), true);
 
+        $locale = $array["geoplugin_countryCode"];
 
-    $ip = $_SERVER['REMOTE_ADDR']; // This will contain the ip of the request
+        if(!$locale){
+            return view('index');
+        }
 
-    // You can use a more sophisticated method to retrieve the content of a webpage with php using a library or something
-    // We will retrieve quickly with the file_get_contents
-    $dataArray = json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip));
+        switch($locale){
+            case "NL":
+                App::setLocale("nl");
+            break;
 
-    // var_dump($dataArray);
+            case "CH":
+            case "DE":
+            case "AT":
+                App::setLocale("de");
+            break;
 
-    $array = json_decode(json_encode($dataArray), true);
-
-    print_r($array);
-    // outputs something like (obviously with the data of your IP) :
-
-    // geoplugin_countryCode => "DE",
-    // geoplugin_countryName => "Germany"
-    // geoplugin_continentCode => "EU"
-
-    // echo "Hello visitor from: ".$array["geoplugin_countryCode"];
-    // return view('index');
+            default;
+                App::setLocale("en");
+            break;
+        }
+        return view('index');
     }
 }
